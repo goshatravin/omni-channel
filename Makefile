1ca49f0e2ff4d04bd9de68f444b72640ea28c792
George Travin <goshatravin@gmail.com>
2020-05-03 21:56:12 +0300

    init

---
+++ Makefile
@@ -0,0 +1,8 @@
+run:
+	npm start
+
+install:
+	npm install
+
+lint:
+	npx eslint .
---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">Hello, world!</div>
+)
+
+export default App;
